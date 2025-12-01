a5e049dfe07227b5e3bf77400aa1c55128c4401a
ryanjfarrell <ryanjf.dev@gmail.com>
2025-12-01 15:49:35 -0800

    solved part 1

--- src/day01/solution.test.ts
+++ src/day01/solution.test.ts
@@ -1,5 +1,11 @@
 import { expect, test } from 'vitest';
-import { solve, parse } from './solution';
+import {
+    solvePart1,
+    parse,
+    getRotationInformation,
+    rotateRight,
+    rotateLeft,
+} from './solution';
 
 const rawInput = `
 L3
@@ -9,4 +15,43 @@ R49
 test('Parsing first as L3, second as R49', () => {
     expect(parse(rawInput)[0]).toBe('L3');
     expect(parse(rawInput)[1]).toBe('R49');
+});
+
+const firstInstruction = parse(rawInput)[0];
+const secondInstruction = parse(rawInput)[1];
+
+test('Test rotation information function', () => {
+    expect(getRotationInformation(firstInstruction)).toEqual({
+        direction: 'L',
+        distance: 3,
+    });
+    expect(getRotationInformation(secondInstruction)).toEqual({
+        direction: 'R',
+        distance: 49,
+    });
+});
+
+test('Rotate right past 99 one to 0', () => {
+    expect(rotateRight(99, 1)).toEqual(0);
+});
+
+test('Rotate left past 0 one to 99', () => {
+    expect(rotateLeft(0, 1)).toEqual(99);
+});
+
+const rawProvided = `
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+`;
+
+test('Sample provided, 3 times at 0', () => {
+    expect(solvePart1(rawProvided)).toEqual(3);
 });
--- src/day01/solution.ts
+++ src/day01/solution.ts
@@ -4,12 +4,96 @@ export function parse(raw: string) {
     return raw.trim().split('\n');
 }
 
-export function solve() {
-    console.log('blah');
-    return 1;
+type Direction = 'L' | 'R';
+
+type Rotation = {
+    direction: Direction;
+    distance: number;
+};
+
+export function getRotationInformation(instruction: string): Rotation {
+    const direction = instruction[0];
+
+    if (direction !== 'L' && direction !== 'R') {
+        throw new Error('Direction is not one of the two allowed values');
+    }
+
+    return {
+        direction,
+        distance: parseInt(instruction.split(direction)[1]),
+    };
+}
+
+// In this problem, 130, 230, etc. all have the same "effective" distance of 30 (the hundredth place just puts you back where you started)
+// Ex. Start 99 - 100 = 99, Start at 99 - 200 = 99
+export function getEffectiveDistance(distance: number) {
+    return distance % 100;
+}
+
+export function rotateRight(
+    currentDialPosition: number,
+    effectiveDistance: number,
+) {
+    const sum = currentDialPosition + effectiveDistance;
+
+    if (sum > 99) {
+        return sum - 100;
+    }
+
+    return sum;
+}
+
+export function rotateLeft(
+    currentDialPosition: number,
+    effectiveDistance: number,
+) {
+    const difference = currentDialPosition - effectiveDistance;
+
+    if (difference < 0) {
+        return difference + 100;
+    }
+
+    return difference;
+}
+
+export function solvePart1(raw: string) {
+    const instructions = parse(raw);
+
+    let currentDialPosition = 50;
+    let timesLandedOnZero = 0;
+
+    instructions.forEach((instruction) => {
+        const { direction, distance } = getRotationInformation(instruction);
+        const effectiveDistance = getEffectiveDistance(distance);
+
+        if (direction === 'L') {
+            currentDialPosition = rotateLeft(
+                currentDialPosition,
+                effectiveDistance,
+            );
+        } else {
+            currentDialPosition = rotateRight(
+                currentDialPosition,
+                effectiveDistance,
+            );
+        }
+
+        if (currentDialPosition === 0) {
+            timesLandedOnZero++;
+        }
+    });
+
+    return timesLandedOnZero;
 }
 
 if (import.meta.url === `file://${process.argv[1]}`) {
     const raw = readInput(__dirname);
-    const parsed = parse(raw);
+    console.log(solvePart1(raw));
 }
+
+/**
+ * Notes:
+ *
+ * Dial starts at 50
+ * left from 5 10 goes to 95
+ */
