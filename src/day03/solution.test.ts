5e36a08877a3ee7b3943a5458e608c174097dd7a
ryanjfarrell <ryanjf.dev@gmail.com>
2025-12-03 10:02:48 -0800

    d3, p1 done

--- src/day03/solution.test.ts
+++ src/day03/solution.test.ts
@@ -1,2 +1,28 @@
 import { expect, test } from 'vitest';
-import { parse } from './solution';
+import { findMaxJoltageInBank, solve } from './solution';
+
+const s1 = '987654321111111';
+const s2 = '811111111111119';
+const s3 = '234234234234278';
+const s4 = '818181911112111';
+
+const samples = [s1, s2, s3, s4];
+
+const samplesWithJoltage = [
+    { bank: s1, j: 98 },
+    { bank: s2, j: 89 },
+    { bank: s3, j: 78 },
+    { bank: s4, j: 92 },
+];
+
+test.each(samplesWithJoltage)(
+    'Bank: %bank, expects max joltage of: %j',
+    ({ bank, j }) => {
+        expect(findMaxJoltageInBank(bank)).toEqual(j);
+    },
+);
+
+const total = samplesWithJoltage.reduce((a, b) => a + b.j, 0);
+
+test('Total joltage in sample', () =>
+    expect(solve(samples.toString()).p1).toEqual(total));
--- src/day03/solution.ts
+++ src/day03/solution.ts
@@ -1,11 +1,51 @@
 import { readInput } from '../utils';
 
 export function parse(raw: string) {
-    return raw.trim().split(',');
+    return raw.trim().split('\n');
 }
 
-export function solve(raw: string) {}
+export function findMaxJoltageInBank(bank: string) {
+    const batteryCount = bank.length;
+    const lastBatteryIndex = batteryCount - 1;
+
+    let first = bank[0];
+    let second = bank[1];
+
+    for (let i = 1; i <= lastBatteryIndex; i++) {
+        const currentBatteryDigitStr = bank[i];
+        const currentBatteryDigitParsed = parseInt(currentBatteryDigitStr);
+
+        if (
+            currentBatteryDigitParsed > parseInt(first) &&
+            i !== lastBatteryIndex
+        ) {
+            first = currentBatteryDigitStr;
+            second = bank[i + 1];
+            continue;
+        }
+
+        if (currentBatteryDigitParsed > parseInt(second)) {
+            second = currentBatteryDigitStr;
+        }
+    }
+
+    return parseInt(first + second);
+}
+
+export function solve(raw: string) {
+    const batteryBanks = parse(raw);
+
+    const totalJoltage = batteryBanks.reduce((acc, bank) => {
+        const maxJoltage = findMaxJoltageInBank(bank);
+
+        return acc + maxJoltage;
+    }, 0);
+
+    return { p1: totalJoltage, p2: 0 };
+}
 
 if (import.meta.url === `file://${process.argv[1]}`) {
     const raw = readInput(__dirname);
+
+    console.log(`Day 3, Part 1, Max Joltage: ${solve(raw).p1}`);
 }
